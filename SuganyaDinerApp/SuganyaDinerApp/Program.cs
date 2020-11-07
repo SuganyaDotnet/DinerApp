d9f47ca3171ceeaf9e45ca0c6f18cbf25bab77ad
Suganya <suganyapselvam1@gmail.com>
2020-11-07 11:06:24 +0530

    Created New Project

---
+++ SuganyaDinerApp/SuganyaDinerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SuganyaDinerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

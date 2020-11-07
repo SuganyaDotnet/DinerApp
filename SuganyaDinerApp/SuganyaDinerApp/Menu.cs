7d6acb43b652e66374843452ef347e6c0b244fd2
Suganya <suganyapselvam1@gmail.com>
2020-11-07 11:13:15 +0530

    Adding Menu and MenuItem Classes

---
+++ SuganyaDinerApp/SuganyaDinerApp/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuganyaDinerApp
+{
+    class Menu
+    {
+        public Menu()
+        {
+            this.MenuItems = new List<MenuItem>();
+        }
+        public string Name { get; set; }
+        public List<MenuItem> MenuItems { get; set; }
+    }
+}
---
+++ SuganyaDinerApp/SuganyaDinerApp/MenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuganyaDinerApp
+{
+    class MenuItem
+    {
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public int Cost { get; set; }
+    }
+}
